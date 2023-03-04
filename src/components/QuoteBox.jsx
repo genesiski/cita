2473f0cd95120c928770ee1f005973c9bdd45db7
genesiski <120064239+genesiski@users.noreply.github.com>
2023-03-03 20:09:42 -0400

    Update QuoteBox.jsx

--- src/components/QuoteBox.jsx
+++ src/components/QuoteBox.jsx
@@ -1,21 +1,18 @@
 import React from 'react'
-import QuoteButton from "./QuoteButton"
+import QuoteButton from './QuoteButton'
 
-const QuoteBox = ({quote,color,newQuote}) => {
+const QuoteBox = ({quote, newQuote, color}) => {
   return (
-    <div className=' Container' style={{color:color}}>
-      <div className='Datingcontainer'>
-        <div><i className='bx bxs-quote-left quote'></i></div>
-        <div> <p>{quote.quote} </p> </div>
-      </div>  
-      <div className='author'>
-      <h3>{quote.author} </h3>
-      </div>
-      <div>
-      <QuoteButton newQuote={newQuote} color={color} />
-      </div>
-     
+    <div className='quoteBox' style={{color: color}}>
+      <p className='quoteBox__quote'>{quote.quote}</p>
+      <h3 className='quoteBox__author' >{quote.author}</h3>
+      <i className='quoteBox__icon bx bxs-quote-left'></i>
+      <QuoteButton
+      color={color} 
+      newQuote={newQuote} 
+      />
     </div>
+    
   )
 }
 
